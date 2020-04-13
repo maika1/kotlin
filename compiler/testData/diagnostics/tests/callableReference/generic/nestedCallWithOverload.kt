2ec7ba670d931de008ed10ba57c632a42dd092b1
Victor Petukhov <victor.petukhov@jetbrains.com>
2020-04-13 11:17:16 +0300

    1

--- compiler/testData/diagnostics/tests/callableReference/generic/nestedCallWithOverload.kt
+++ compiler/testData/diagnostics/tests/callableReference/generic/nestedCallWithOverload.kt
@@ -1,20 +1,18 @@
 // !WITH_NEW_INFERENCE
-// !DIAGNOSTICS: -UNUSED_VARIABLE, -UNUSED_PARAMETER
+// !DIAGNOSTICS: -UNUSED_VARIABLE, -UNUSED_PARAMETER -UNUSED_ANONYMOUS_PARAMETER
 
 fun foo(i: Int) {}
 fun foo(s: String) {}
-fun <K> id1(x: K): K = x
 fun <K> id(x: K): K = x
+fun <K> id1(x: K): K = x
 fun <L> id2(x: L): L = x
 fun <T> baz(x: T, y: T): T = TODO()
 
-fun test() {
+fun test1() {
     val x1: (Int) -> Unit = id(id(::foo))
     val x2: (Int) -> Unit = baz(id(::foo), ::foo)
     val x3: (Int) -> Unit = baz(id(::foo), id(id(::foo)))
     val x4: (String) -> Unit = baz(id(::foo), id(id(::foo)))
-    val x5: (Double) -> Unit = baz(id(::<!NI;CALLABLE_REFERENCE_RESOLUTION_AMBIGUITY, OI;NONE_APPLICABLE!>foo<!>), id(id(::<!NI;CALLABLE_REFERENCE_RESOLUTION_AMBIGUITY, OI;NONE_APPLICABLE!>foo<!>)))
-
 
     id<(Int) -> Unit>(id(id(::foo)))
     id(id<(Int) -> Unit>(::foo))
